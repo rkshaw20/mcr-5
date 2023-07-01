f7323d5b34018eebdcc9e826de9c84bfdd5fe2d2
rkshaw20 <rkshaw2026@gmail.com>
2023-07-01 17:10:04 +0530

    initial

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import { Flex, Heading } from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+
+const Home=()=>{
+    return (
+        <Flex>
+       < Heading>Home</Heading>
+<ColorModeSwitcher/>
+        </Flex>
+    )
+}
+
+export default Home;
